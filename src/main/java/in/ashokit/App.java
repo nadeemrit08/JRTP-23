7c9d34860e7d0e92660ec08456c18da26591020e
Nadeem JRTP <nadeemrit08@gmail.com>
2024-05-17 10:20:48 +0530

    added i var

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -8,11 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
+
 		int i = 10;
-=======
+
         String s ="hi";
->>>>>>> 16fc7bb2d2cff49f08b227c381c5c8a3e29b02e7
         System.out.println( "Hello World!" );
     }
 }
